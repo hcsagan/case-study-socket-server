bbb7fcbf55c76db66046dfe1774c14f5881899cf
hacsa <hacsa@jfmedier.dk>
2022-12-05 00:59:52 +0100

    created socket server

---
+++ index.js
@@ -0,0 +1,47 @@
+const httpServer = require("http").createServer();
+const https = require("https");
+const { clearInterval } = require("timers");
+const io = require("socket.io")(httpServer, {
+  // ...
+});
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(3000);
+
+const getRandomUser = () =>
+  new Promise((resolve, reject) => {
+    https
+      .get("https://randomuser.me/api/", (resp) => {
+        let data = "";
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        resp.on("end", () => {
+          resolve(data);
+        });
+      })
+      .on("error", (err) => {
+        reject(data.message);
+      });
+  });
+
+io.on("connection", async (socket) => {
+  const loop = setInterval(async () => {
+    try {
+      const user = await getRandomUser();
+
+      socket.emit("userList", JSON.parse(user));
+    } catch (e) {
+      console.log(e);
+    }
+  }, 5000);
+
+  socket.on("disconnect", () => {
+    clearInterval(loop);
+    console.log("user disconnected");
+  });
+});
