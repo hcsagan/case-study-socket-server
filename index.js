239c10ec8c8976b224b68b0778efe6a34458e72f
hacsa <hacsa@jfmedier.dk>
2022-12-05 01:34:43 +0100

    installed express

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const https = require("https");
-const httpServer = https.createServer();
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 
 const { clearInterval } = require("timers");
-const io = require("socket.io")(httpServer, {});
+const io = require("socket.io")(server);
 
 httpServer.listen(80);
 
